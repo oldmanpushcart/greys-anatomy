1fea5cdd06f6dc4126461355acd63a176ac9a90e
vlinux <oldmanpushcart@gmail.com>
2012-11-24 01:22:49 +0800

    添加注释

--- src/main/java/com/googlecode/greysanatomy/probe/JobListener.java
+++ src/main/java/com/googlecode/greysanatomy/probe/JobListener.java
@@ -1,8 +1,19 @@
 package com.googlecode.greysanatomy.probe;
 
+/**
+ * 任务监听器
+ * @author vlinux
+ *
+ */
 public interface JobListener {
 
+	/**
+	 * 监听器创建
+	 */
 	void create();
-
+	
+	/**
+	 * 监听器销毁
+	 */
 	void destroy();
 }
