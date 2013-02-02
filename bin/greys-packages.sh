d95e8167d4238fa1f20d7e87168ec43a2af18bfc
vlinux <oldmanpushcart@gmail.com>
2013-02-02 15:09:15 +0800

    init

--- bin/greys-packages.sh
+++
@@ -1,8 +0,0 @@
-./package.sh
-mkdir -p ../target/greys
-cp greys ../target/greys
-cp ../target/greys-anatomy-jar-with-dependencies.jar ../target/greys/greys.jar
-cd ../target/
-zip -r greys.zip greys/
-cd -
-
--- bin/package.sh
+++
@@ -1,1 +0,0 @@
-mvn clean package -Dmaven.test.skip=true -f ../pom.xml
