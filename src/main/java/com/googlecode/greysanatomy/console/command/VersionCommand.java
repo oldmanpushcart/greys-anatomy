e13fe5489a207179820002d6094d13dd4d6984fc
vlinux <oldmanpushcart@gmail.com>
2013-08-24 09:24:20 +0800

    查看版本号命令

---
+++ src/main/java/com/googlecode/greysanatomy/console/command/VersionCommand.java
@@ -0,0 +1,26 @@
+package com.googlecode.greysanatomy.console.command;
+
+import com.googlecode.greysanatomy.console.command.annotation.Cmd;
+import com.googlecode.greysanatomy.util.GaStringUtils;
+
+/**
+ * Êä³ö°æ±¾
+ * @author vlinux
+ *
+ */
+@Cmd("version")
+public class VersionCommand extends Command {
+
+	@Override
+	public Action getAction() {
+		return new Action(){
+
+			@Override
+			public void action(final Info info, final Sender sender) throws Throwable {
+				sender.send(true, GaStringUtils.getLogo());
+			}
+			
+		};
+	}
+
+}
