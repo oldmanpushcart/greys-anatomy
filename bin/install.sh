00567e06d08b267b5378456b3679bd62cbc2638c
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-10-26 19:18:56 +0800

    更新安装脚本

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,7 @@
 typeset GREYS_FILE="greys.zip";
 
 echo "Download... greys.zip";
-curl -sLk "http://greys.oss.aliyuncs.com/greys.zip?file=greys.zip" -o $GREYS_FILE;
+curl -sLk "http://ompc.oss-cn-hangzhou.aliyuncs.com/greys/greys.zip" -o $GREYS_FILE;
 if [[ ! $? -eq  0 ]]; then
     echo "download file failed!";
     exit -1;
