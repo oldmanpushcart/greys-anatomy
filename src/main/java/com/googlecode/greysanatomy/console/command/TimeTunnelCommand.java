7e810447de656383844f1de19845d94e0277728b
oldmanpushcart@gmail.com <oldmanpushcart@gmail.com>
2014-11-19 10:16:47 +0800

    更名为TimeTunnel

--- src/main/java/com/googlecode/greysanatomy/console/command/TimeTunnelCommand.java
+++ src/main/java/com/googlecode/greysanatomy/console/command/TimeTunnelCommand.java
@@ -32,33 +32,33 @@ import static java.lang.String.format;
 import static org.apache.commons.lang.StringUtils.*;
 
 /**
- * 记录命令<br/>
+ * 时光隧道命令<br/>
  * 参数w/d依赖于参数i所传递的记录编号<br/>
  * Created by vlinux on 14/11/15.
  */
-@RiscCmd(named = "record", sort = 8, desc = "Recording the method call.",
+@RiscCmd(named = "tt", sort = 8, desc = "TimeTunnel the method call.",
         eg = {
-                "record -r .*StringUtils isEmpty",
-                "record -l",
-                "record -D",
-                "record -i 1000 -w p.params[0]",
-                "record -i 1000 -d",
-                "record -i 1000",
-//                "record -i 1000 -p"
+                "tt -r .*StringUtils isEmpty",
+                "tt -l",
+                "tt -D",
+                "tt -i 1000 -w p.params[0]",
+                "tt -i 1000 -d",
+                "tt -i 1000",
+//                "tt -i 1000 -p"
         })
-public class RecordCommand extends Command {
+public class TimeTunnelCommand extends Command {
 
     private final Logger logger = LoggerFactory.getLogger("greysanatomy");
 
-    // the records collection
-    private static final Map<Integer, Record> records = new LinkedHashMap<Integer, Record>();
+    // the TimeTunnels collection
+    private static final Map<Integer, TimeTunnel> timeTunnels = new LinkedHashMap<Integer, TimeTunnel>();
 
-    // the record's index sequence
+    // the TimeTunnel's index sequence
     private static final AtomicInteger sequence = new AtomicInteger(1000);
 
-    // record the method call
-    @RiscNamedArg(named = "r", description = "record the method called.")
-    private boolean isRecord = false;
+    // TimeTunnel the method call
+    @RiscNamedArg(named = "t", description = "TimeTunnel the method called.")
+    private boolean isTimeTunnel = false;
 
     @RiscIndexArg(index = 0, isRequired = false, name = "class-regex", description = "regex match of classpath.classname")
     private String classRegex;
@@ -66,22 +66,22 @@ public class RecordCommand extends Command {
     @RiscIndexArg(index = 1, isRequired = false, name = "method-regex", description = "regex match of methodname")
     private String methodRegex;
 
-    // list the record
-    @RiscNamedArg(named = "l", description = "list all the records.")
+    // list the TimeTunnel
+    @RiscNamedArg(named = "l", description = "list all the TimeTunnels.")
     private boolean isList = false;
 
-    @RiscNamedArg(named = "D", description = "delete all records.")
+    @RiscNamedArg(named = "D", description = "delete all TimeTunnels.")
     private boolean isDeleteAll = false;
 
 
-    // index of record
-    @RiscNamedArg(named = "i", hasValue = true, description = "appoint the index of record. If use only, show the record detail.")
+    // index of TimeTunnel
+    @RiscNamedArg(named = "i", hasValue = true, description = "appoint the index of TimeTunnel. If use only, show the TimeTunnel detail.")
     private Integer index;
 
-    // watch the index record
+    // watch the index TimeTunnel
     @RiscNamedArg(named = "w",
             hasValue = true,
-            description = "watch the record's data, like p.params[0], p.returnObj, p.throwExp and so on.",
+            description = "watch the TimeTunnel's data, like p.params[0], p.returnObj, p.throwExp and so on.",
             description2 = ""
                     + " \n"
                     + "For example\n"
@@ -103,12 +103,12 @@ public class RecordCommand extends Command {
     private String watchExpress = StringUtils.EMPTY;
 
 
-//    // play the index record
-//    @RiscNamedArg(named = "p", description = "play the record of method called.")
+//    // play the index TimeTunnel
+//    @RiscNamedArg(named = "p", description = "play the TimeTunnel of method called.")
 //    private boolean isPlay = false;
 
-    // delete the index record
-    @RiscNamedArg(named = "d", description = "delete the index record.")
+    // delete the index TimeTunnel
+    @RiscNamedArg(named = "d", description = "delete the index TimeTunnel.")
     private boolean isDelete = false;
 
 
@@ -123,13 +123,13 @@ public class RecordCommand extends Command {
                 || isDelete) {
 
             if (null == index) {
-                throw new IllegalArgumentException("miss record index, please type -i to appoint it.");
+                throw new IllegalArgumentException("miss TimeTunnel index, please type -i to appoint it.");
             }
 
         }
 
         // 在r参数下class-regex,method-regex由选填变成必填
-        if (isRecord) {
+        if (isTimeTunnel) {
             if (StringUtils.isBlank(classRegex)) {
                 throw new IllegalArgumentException("miss class-regex, please type the regex express to match class.");
             }
@@ -151,7 +151,7 @@ public class RecordCommand extends Command {
 
         // 一个参数都没有是不行滴
         if (null == index
-                && !isRecord
+                && !isTimeTunnel
                 && !isDelete
                 && !isDeleteAll
                 && StringUtils.isBlank(watchExpress)
@@ -159,21 +159,21 @@ public class RecordCommand extends Command {
 //                && !isPlay
                 ) {
 
-            throw new IllegalArgumentException("miss arguments, type help record to got usage.");
+            throw new IllegalArgumentException("miss arguments, type help TimeTunnel to got usage.");
 
         }
 
     }
 
     /**
-     * 创建record
+     * 创建TimeTunnel
      *
-     * @param record
+     * @param timeTunnel
      * @return
      */
-    private int putRecord(Record record) {
+    private int putTimeTunnel(TimeTunnel timeTunnel) {
         final int index = sequence.getAndIncrement();
-        records.put(index, record);
+        timeTunnels.put(index, timeTunnel);
         return index;
     }
 
@@ -250,9 +250,9 @@ public class RecordCommand extends Command {
      *
      * @param lineSB
      * @param index
-     * @param record
+     * @param timeTunnel
      */
-    private void printRecord(StringBuilder lineSB, int index, Record record) {
+    private void printTimeTunnel(StringBuilder lineSB, int index, TimeTunnel timeTunnel) {
 
         final StringBuilder lineFormatSB = new StringBuilder();
         for (int colWidth : TABLE_COL_WIDTH) {
@@ -263,25 +263,25 @@ public class RecordCommand extends Command {
         final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         lineSB.append(format(lineFormat,
                 index,
-                sdf.format(record.getGmtCreate()),
-                record.getAdvice().isReturn(),
-                record.getAdvice().isThrowException(),
-                record.getAdvice().getTarget().getTargetThis() == null ? "NULL" : "0x" + Integer.toHexString(record.getAdvice().getTarget().getTargetThis().hashCode()),
-//                substring(record.getTargetClassLoader().getClass().getSimpleName(), 0, TABLE_COL_WIDTH[4]),
-                summary(substring(substringAfterLast(record.getAdvice().getTarget().getTargetClassName(), "."), 0, TABLE_COL_WIDTH[5]), TABLE_COL_WIDTH[5]),
-                summary(substring(record.getAdvice().getTarget().getTargetBehaviorName(), 0, TABLE_COL_WIDTH[6]), TABLE_COL_WIDTH[6])
+                sdf.format(timeTunnel.getGmtCreate()),
+                timeTunnel.getAdvice().isReturn(),
+                timeTunnel.getAdvice().isThrowException(),
+                timeTunnel.getAdvice().getTarget().getTargetThis() == null ? "NULL" : "0x" + Integer.toHexString(timeTunnel.getAdvice().getTarget().getTargetThis().hashCode()),
+//                substring(TimeTunnel.getTargetClassLoader().getClass().getSimpleName(), 0, TABLE_COL_WIDTH[4]),
+                summary(substring(substringAfterLast(timeTunnel.getAdvice().getTarget().getTargetClassName(), "."), 0, TABLE_COL_WIDTH[5]), TABLE_COL_WIDTH[5]),
+                summary(substring(timeTunnel.getAdvice().getTarget().getTargetBehaviorName(), 0, TABLE_COL_WIDTH[6]), TABLE_COL_WIDTH[6])
         )).append("\n");
 
     }
 
     /**
-     * do the record command
+     * do the TimeTunnel command
      *
      * @param info
      * @param sender
      * @throws Throwable
      */
-    private void doRecord(final Info info, final Sender sender) throws Throwable {
+    private void doTimeTunnel(final Info info, final Sender sender) throws Throwable {
 
         final Instrumentation inst = info.getInst();
         final GreysAnatomyClassFileTransformer.TransformResult result = transform(inst, classRegex, methodRegex, new AdviceListenerAdapter() {
@@ -293,8 +293,8 @@ public class RecordCommand extends Command {
 
                 try {
 
-                    final Record record = new Record(advice, new Date());
-                    final int index = putRecord(record);
+                    final TimeTunnel timeTunnel = new TimeTunnel(advice, new Date());
+                    final int index = putTimeTunnel(timeTunnel);
 
                     final StringBuilder lineSB = new StringBuilder();
                     if (isFirst) {
@@ -303,12 +303,12 @@ public class RecordCommand extends Command {
                         printTableHead(lineSB);
                     }
 
-                    printRecord(lineSB, index, record);
+                    printTimeTunnel(lineSB, index, timeTunnel);
 
                     sender.send(false, lineSB.toString());
 
                 } catch (Throwable t) {
-                    logger.warn("record failed.", t);
+                    logger.warn("TimeTunnel failed.", t);
                 }
 
             }
@@ -332,7 +332,7 @@ public class RecordCommand extends Command {
 
 
     /**
-     * do list records
+     * do list timeTunnels
      *
      * @param sender
      * @throws Throwable
@@ -340,12 +340,12 @@ public class RecordCommand extends Command {
     private void doList(final Sender sender) throws Throwable {
 
         final StringBuilder lineSB = new StringBuilder();
-        if (records.isEmpty()) {
-            lineSB.append("records is empty.\n");
+        if (timeTunnels.isEmpty()) {
+            lineSB.append("timeTunnels is empty.\n");
         } else {
             printTableHead(lineSB);
-            for (Map.Entry<Integer, Record> entry : records.entrySet()) {
-                printRecord(lineSB, entry.getKey(), entry.getValue());
+            for (Map.Entry<Integer, TimeTunnel> entry : timeTunnels.entrySet()) {
+                printTimeTunnel(lineSB, entry.getKey(), entry.getValue());
             }
             printLineSplit(lineSB);
 
@@ -361,8 +361,8 @@ public class RecordCommand extends Command {
      * @param sender
      */
     private void doDeleteAll(final Sender sender) {
-        records.clear();
-        sender.send(true, "All records was deleted.\n");
+        timeTunnels.clear();
+        sender.send(true, "All timeTunnels was deleted.\n");
     }
 
     /**
@@ -373,10 +373,10 @@ public class RecordCommand extends Command {
      */
     private void doWatch(final Sender sender) throws Throwable {
 
-        // find the record
-        final Record record = records.get(index);
-        if (null == record) {
-            sender.send(true, format("record %s not found.", index));
+        // find the TimeTunnel
+        final TimeTunnel timeTunnel = timeTunnels.get(index);
+        if (null == timeTunnel) {
+            sender.send(true, format("TimeTunnel %s not found.", index));
             return;
         }
 
@@ -384,7 +384,7 @@ public class RecordCommand extends Command {
 
         jsEngine.eval("function printWatch(p,o){try{o.send(true, " + watchExpress + "+'\\n');}catch(e){o.send(true, e.message+'\\n');}}");
         final Invocable invoke = (Invocable) jsEngine;
-        final Advice p = record.getAdvice();
+        final Advice p = timeTunnel.getAdvice();
         invoke.invokeFunction("printWatch", p, sender);
 
     }
@@ -397,7 +397,7 @@ public class RecordCommand extends Command {
      */
     private void doDelete(final Sender sender) {
 
-        records.remove(index);
+        timeTunnels.remove(index);
         sender.send(true, format("delete %s successed.", index));
 
     }
@@ -411,7 +411,7 @@ public class RecordCommand extends Command {
 //    private void doPlay(final Sender sender) throws Throwable {
 //
 //        // find the record
-//        final Record record = records.get(index);
+//        final Record record = timeTunnels.get(index);
 //        if (null == record) {
 //            sender.send(true, format("record %s not found.", index));
 //            return;
@@ -437,16 +437,16 @@ public class RecordCommand extends Command {
      */
     private void doShow(final Sender sender) {
 
-        // find the record
-        final Record record = records.get(index);
-        if (null == record) {
-            sender.send(true, format("record %s not found.", index));
+        // find the TimeTunnel
+        final TimeTunnel timeTunnel = timeTunnels.get(index);
+        if (null == timeTunnel) {
+            sender.send(true, format("TimeTunnel %s not found.", index));
             return;
         }
 
-        final String className = record.getAdvice().getTarget().getTargetClassName();
-        final String methodName = record.getAdvice().getTarget().getTargetBehaviorName();
-        final String objectAddress = record.getAdvice().getTarget().getTargetThis() == null ? "NULL" : "0x" + Integer.toHexString(record.getAdvice().getTarget().getTargetThis().hashCode());
+        final String className = timeTunnel.getAdvice().getTarget().getTargetClassName();
+        final String methodName = timeTunnel.getAdvice().getTarget().getTargetBehaviorName();
+        final String objectAddress = timeTunnel.getAdvice().getTarget().getTargetThis() == null ? "NULL" : "0x" + Integer.toHexString(timeTunnel.getAdvice().getTarget().getTargetThis().hashCode());
         final int maxColLen = Math.max(Math.max(className.length(), methodName.length()), 50);
 
         final StringBuilder detailSB = new StringBuilder();
@@ -462,9 +462,9 @@ public class RecordCommand extends Command {
         // fill the head
         detailSB.append(lineSplit)
                 .append(format(headFormat, "INDEX: ", index)).append("\n")
-                .append(format(headFormat, "GMT-CREATE: ", sdf.format(record.getGmtCreate()))).append("\n")
-                .append(format(headFormat, "IS-RETURN: ", record.getAdvice().isReturn())).append("\n")
-                .append(format(headFormat, "IS-EXCEPTION: ", record.getAdvice().isThrowException())).append("\n")
+                .append(format(headFormat, "GMT-CREATE: ", sdf.format(timeTunnel.getGmtCreate()))).append("\n")
+                .append(format(headFormat, "IS-RETURN: ", timeTunnel.getAdvice().isReturn())).append("\n")
+                .append(format(headFormat, "IS-EXCEPTION: ", timeTunnel.getAdvice().isThrowException())).append("\n")
                 .append(format(headFormat, "OBJECT: ", objectAddress)).append("\n")
                 .append(format(headFormat, "CLASS: ", className)).append("\n")
                 .append(format(headFormat, "METHOD: ", methodName)).append("\n")
@@ -473,10 +473,10 @@ public class RecordCommand extends Command {
 
 
         // fill the paramenters
-        if (null != record.getAdvice().getParameters()) {
+        if (null != timeTunnel.getAdvice().getParameters()) {
 
             int paramIndex = 0;
-            for (Object param : record.getAdvice().getParameters()) {
+            for (Object param : timeTunnel.getAdvice().getParameters()) {
                 detailSB.append("PARAMETERS[" + paramIndex++ + "]:\n").append(param).append("\n\n");
             }
 
@@ -484,14 +484,14 @@ public class RecordCommand extends Command {
 
 
         // fill the returnObj
-        if (record.getAdvice().isReturn()) {
-            detailSB.append("RETURN-OBJ:\n").append(record.getAdvice().getReturnObj()).append("\n\n");
+        if (timeTunnel.getAdvice().isReturn()) {
+            detailSB.append("RETURN-OBJ:\n").append(timeTunnel.getAdvice().getReturnObj()).append("\n\n");
         }
 
 
         // fill the throw exception
-        if (record.getAdvice().isThrowException()) {
-            final Throwable throwable = record.getAdvice().getThrowException();
+        if (timeTunnel.getAdvice().isThrowException()) {
+            final Throwable throwable = timeTunnel.getAdvice().getThrowException();
             final StringWriter stringWriter = new StringWriter();
             final PrintWriter printWriter = new PrintWriter(stringWriter);
             throwable.printStackTrace(printWriter);
@@ -513,9 +513,9 @@ public class RecordCommand extends Command {
                 checkArguments();
 
 
-                // record 命令
-                if (isRecord) {
-                    doRecord(info, sender);
+                // TimeTunnel 命令
+                if (isTimeTunnel) {
+                    doTimeTunnel(info, sender);
                 } else if (isList) {
 
                     // list 命令
@@ -523,28 +523,28 @@ public class RecordCommand extends Command {
 
                 } else if (isDeleteAll) {
 
-                    // delete all the record
+                    // delete all the TimeTunnel
                     doDeleteAll(sender);
 
                 } else if (StringUtils.isNotBlank(watchExpress)) {
 
-                    // watch record by js express
+                    // watch TimeTunnel by js express
                     doWatch(sender);
 
                 } else if (isDelete) {
 
-                    // delete index record
+                    // delete index TimeTunnel
                     doDelete(sender);
 
                 } else if (null != index) {
 
-                    // show the record
+                    // show the TimeTunnel
                     doShow(sender);
 
                 }
 //                else if (isPlay) {
 //
-//                    // play the record
+//                    // play the TimeTunnel
 //                    doPlay(sender);
 //
 //                }
@@ -561,9 +561,9 @@ public class RecordCommand extends Command {
 /**
  * 探测记录方法调用记录
  */
-class Record {
+class TimeTunnel {
 
-    public Record(Advice advice, Date gmtCreate) {
+    public TimeTunnel(Advice advice, Date gmtCreate) {
         this.advice = advice;
         this.gmtCreate = gmtCreate;
     }
