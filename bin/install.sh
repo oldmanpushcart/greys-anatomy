9dfeaebce599397a961bebe97a94747ce1057ad4
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2015-06-15 23:50:05 +0800

    更新安装脚本

--- bin/install.sh
+++ bin/install.sh
@@ -1,21 +1,38 @@
 #! /bin/bash
 
-GREYS_FILE="greys.zip";
+# temp greys file
+TEMP_GREYS_FILE="./greys.zip.$$"
 
+# check permission to download && install
 if [ ! -w ./ ]; then
-    echo "permission denied, ./ was not writable." >> /dev/stderr
-    exit 1;
+    echo "permission denied, target directory is not writable." >> /dev/stderr
+    exit 1
 fi
 
-echo "Downloading... ${GREYS_FILE}";
-curl -sLk "http://ompc.oss.aliyuncs.com/greys/greys.zip" -o ${GREYS_FILE};
+
+
+# download from aliyunos
+echo "downloading... ${TEMP_GREYS_FILE}";
+curl -sLk "http://ompc.oss.aliyuncs.com/greys/greys.zip" -o ${TEMP_GREYS_FILE};
 if [ ! $? -eq 0 ]; then
     echo "download failed!" >> /dev/stderr
-    exit 1;
+    exit 1
 fi
 
-unzip ${GREYS_FILE}
-rm -rf ${GREYS_FILE}
+
+# clean if exists
+rm -rf ./greys
+
+# unzip the greys file
+unzip ${TEMP_GREYS_FILE}
+if [ ! $? -eq 0 ]; then
+    echo "file damage!" >> /dev/stderr
+    exit 1
+fi
+
+# install greys
+rm -rf ${TEMP_GREYS_FILE}
 chmod +x greys/greys.sh
 
+# done
 echo "greys install successed."
