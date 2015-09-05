7d269852b24759fc0fcd16d04e0025656d51e246
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2015-09-05 20:38:35 +0800

    优化命令行解析代码，让命令行解析不再依赖引号强制引用才能生效，更贴近平时使用习惯

--- core/src/main/java/com/github/ompc/greys/core/util/GaStringUtils.java
+++ core/src/main/java/com/github/ompc/greys/core/util/GaStringUtils.java
@@ -7,7 +7,6 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 
@@ -27,6 +26,15 @@ public class GaStringUtils {
      */
     public static final String ABORT_MSG = "Press Ctrl+D or Ctrl+X to abort.";
 
+
+    /**
+     * 解析状态
+     */
+    private enum SPLIT_FOR_ARGUMENT_STATE {
+        ESCAPE_CHAR,
+        READ_CHAR
+    }
+
     /**
      * 拆分参数，要求能将命令行字符串拆分成为多个数组
      *
@@ -35,26 +43,101 @@ public class GaStringUtils {
      */
     public static String[] splitForArgument(String argumentString) {
 
-        Pattern compile = Pattern.compile("(\"[^\"]*\")|('[^']*')|([^\\s+]+)");
-        java.util.regex.Matcher matcher = compile.matcher(argumentString);
-
         final ArrayList<String> stringList = new ArrayList<String>();
-        while (matcher.find()) {
 
-            final String segment = matcher.group();
-            if (segment.length() > 1
-                    && ((segment.startsWith("'") && segment.endsWith("'")) || (segment.startsWith("\"") && segment.endsWith("\"")))) {
-                stringList.add(segment.replaceAll("(^['|\"])|(['|\"]$)", ""));
-            } else {
-                stringList.add(segment);
+        if (StringUtils.isNotBlank(argumentString)) {
+
+            // 字符串片段
+            StringBuilder segmentSB = new StringBuilder();
+
+            // 解析状态
+            SPLIT_FOR_ARGUMENT_STATE state = SPLIT_FOR_ARGUMENT_STATE.READ_CHAR;
+
+            // 期待片段分隔符
+            char splitSegmentChar = ' ';
+
+            // 是否在片段中
+            boolean isInSegment = true;
+
+
+            for (char c : argumentString.toCharArray()) {
+
+                switch (state) {
+
+                    case READ_CHAR: {
+
+                        // 匹配到转义符时，任何时候都跳转到转义处理
+                        if (GaCheckUtils.isEquals(c, '\\')) {
+                            state = SPLIT_FOR_ARGUMENT_STATE.ESCAPE_CHAR;
+                            break;
+                        }
+
+                        // 段落中的匹配
+                        if (isInSegment) {
+
+                            // 如果匹配到片段结束分隔符，则结束当前片段
+                            if (GaCheckUtils.isEquals(c, splitSegmentChar)) {
+                                final String segmentString = segmentSB.toString();
+                                if (StringUtils.isNotBlank(segmentString)) {
+                                    stringList.add(segmentString);
+                                }
+                                segmentSB = new StringBuilder();
+                                isInSegment = false;
+                            }
+
+                            // 其他情况则一律添加到片段中
+                            else {
+                                segmentSB.append(c);
+                            }
+
+                        }
+
+                        // 非段落中的匹配
+                        else {
+
+                            // 过滤掉连续空格
+                            if (GaCheckUtils.isEquals(c, ' ')) {
+                                break;
+                            }
+
+                            // 命中片段分隔符
+                            else if (GaCheckUtils.isIn(c, '\'', '"')) {
+                                splitSegmentChar = c;
+                                isInSegment = true;
+                            }
+
+                            // 其他字符则一律添加到片段中
+                            // 同时认为以空格作为片段分隔符
+                            else {
+                                splitSegmentChar = ' ';
+                                isInSegment = true;
+                                segmentSB.append(c);
+                            }
+
+                        }
+
+                        break;
+                    }
+
+                    case ESCAPE_CHAR: {
+                        segmentSB.append(c);
+                        state = SPLIT_FOR_ARGUMENT_STATE.READ_CHAR;
+                        break;
+                    }
+                }//switch
+
+            }//for
+
+            // 最后循环结束需要强制提交片段
+            final String segmentString = segmentSB.toString();
+            if (StringUtils.isNotBlank(segmentString)) {
+                stringList.add(segmentString);
             }
-
         }
 
         return stringList.toArray(new String[stringList.size()]);
 
     }
-
 
     /**
      * 获取异常的原因描述
