d0b40e2bde8579aef31e7f6a7d002ff4b1eb892d
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2015-10-20 17:49:44 +0800

    大概调整了下 #80 修复的代码

--- core/src/main/java/com/github/ompc/greys/core/view/KVView.java
+++ core/src/main/java/com/github/ompc/greys/core/view/KVView.java
@@ -1,8 +1,8 @@
 package com.github.ompc.greys.core.view;
 
-import java.util.Scanner;
-
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
 
 /**
  * KV排版控件
@@ -39,22 +39,39 @@ public class KVView implements View {
 
     @Override
     public String draw() {
-        String content = tableView.draw();
-        StringBuilder sb = new StringBuilder();
-        // 清理多余的空格
-        Scanner scanner = new Scanner(content);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (line != null) {
-                //清理一行后面多余的空格
-                line = StringUtils.stripEnd(line, " ");
-                if(line.isEmpty()){
-                    line = " ";
+        return filterEmptyLine(tableView.draw());
+    }
+
+
+    /*
+     * 出现多余的空行的原因是，KVview在输出时，会补全空格到最长的长度。所以在"yyyyy”后面会多出来很多的空格。
+     * 再经过TableView的固定列处理，多余的空格就会在一行里放不下，输出成两行（第二行前面是空格）
+     *
+     * @see https://github.com/oldmanpushcart/greys-anatomy/issues/82
+     */
+    private String filterEmptyLine(String content) {
+        final StringBuilder sb = new StringBuilder();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(content);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line != null) {
+                    //清理一行后面多余的空格
+                    line = StringUtils.stripEnd(line, " ");
+                    if (line.isEmpty()) {
+                        line = " ";
+                    }
                 }
+                sb.append(line).append('\n');
             }
-            sb.append(line).append('\n');
+        } finally {
+            if (null != scanner) {
+                scanner.close();
+            }
         }
-        scanner.close();
+
         return sb.toString();
     }
+
 }
