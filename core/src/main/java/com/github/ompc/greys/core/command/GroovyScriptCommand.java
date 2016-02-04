32e9cc24bb7ab4f46c79db81508ae7d64b76b50a
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2016-02-04 11:13:39 +0800

    修复#116问题

--- core/src/main/java/com/github/ompc/greys/core/command/GroovyScriptCommand.java
+++ core/src/main/java/com/github/ompc/greys/core/command/GroovyScriptCommand.java
@@ -1,8 +1,19 @@
 package com.github.ompc.greys.core.command;
 
+import com.github.ompc.greys.core.Advice;
+import com.github.ompc.greys.core.advisor.AdviceListener;
+import com.github.ompc.greys.core.advisor.InnerContext;
+import com.github.ompc.greys.core.advisor.ProcessContext;
+import com.github.ompc.greys.core.advisor.ReflectAdviceListenerAdapter;
+import com.github.ompc.greys.core.command.annotation.Cmd;
 import com.github.ompc.greys.core.command.annotation.IndexArg;
 import com.github.ompc.greys.core.command.annotation.NamedArg;
 import com.github.ompc.greys.core.server.Session;
+import com.github.ompc.greys.core.util.PointCut;
+import com.github.ompc.greys.core.util.matcher.ClassMatcher;
+import com.github.ompc.greys.core.util.matcher.GaMethodMatcher;
+import com.github.ompc.greys.core.util.matcher.PatternMatcher;
+import groovy.lang.GroovyClassLoader;
 
 import java.io.File;
 import java.lang.instrument.Instrumentation;
@@ -13,13 +24,12 @@ import static org.apache.commons.lang3.StringUtils.EMPTY;
  * 脚本增强命令
  * Created by oldmanpushcart@gmail.com on 15/5/31.
  */
-//@Cmd(name = "groovy", sort = 6, summary = "Enhanced Groovy",
-//        eg = {
-//                "groovy -E org\\.apache\\.commons\\.lang\\.StringUtils isBlank /tmp/watch.groovy",
-//                "groovy org.apache.commons.lang.StringUtils isBlank /tmp/watch.groovy",
-//                "groovy *StringUtils isBlank /tmp/watch.groovy"
-//        })
-@Deprecated
+@Cmd(name = "groovy", sort = 6, summary = "Enhanced Groovy",
+        eg = {
+                "groovy -E org\\.apache\\.commons\\.lang\\.StringUtils isBlank /tmp/watch.groovy",
+                "groovy org.apache.commons.lang.StringUtils isBlank /tmp/watch.groovy",
+                "groovy *StringUtils isBlank /tmp/watch.groovy"
+        })
 public class GroovyScriptCommand implements ScriptSupportCommand, Command {
 
     @IndexArg(index = 0, name = "class-pattern", summary = "Path and classname of Pattern Matching")
@@ -74,55 +84,53 @@ public class GroovyScriptCommand implements ScriptSupportCommand, Command {
                     }
                 };
 
-                return null;
+                final GroovyClassLoader groovyClassLoader = new GroovyClassLoader(getClass().getClassLoader());
+                final Class<?> scriptClass = groovyClassLoader.parseClass(scriptFile);
+                final ScriptListener scriptListener = (ScriptListener) scriptClass.newInstance();
 
-//                final Class<?> scriptClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(scriptFile);
-//                final ScriptListener scriptListener = (ScriptListener) scriptClass.newInstance();
-//
-//
-//                return new GetEnhancer() {
-//                    @Override
-//                    public Matcher getClassNameMatcher() {
-//                        return classNameMatcher;
-//                    }
-//
-//                    @Override
-//                    public Matcher getMethodNameMatcher() {
-//                        return methodNameMatcher;
-//                    }
-//
-//                    @Override
-//                    public AdviceListener getAdviceListener() {
-//
-//                        return new DefaultReflectAdviceListenerAdapter() {
-//
-//                            @Override
-//                            public void create() {
-//                                scriptListener.create(output);
-//                            }
-//
-//                            @Override
-//                            public void destroy() {
-//                                scriptListener.destroy(output);
-//                            }
-//
-//                            @Override
-//                            public void before(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
-//                                scriptListener.before(output, advice);
-//                            }
-//
-//                            @Override
-//                            public void afterReturning(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
-//                                scriptListener.afterReturning(output, advice);
-//                            }
-//
-//                            @Override
-//                            public void afterThrowing(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
-//                                scriptListener.afterThrowing(output, advice);
-//                            }
-//                        };
-//                    }
-//                };
+                return new GetEnhancer() {
+
+                    @Override
+                    public PointCut getPointCut() {
+                        return new PointCut(
+                                new ClassMatcher(new PatternMatcher(isRegEx, classPattern)),
+                                new GaMethodMatcher(new PatternMatcher(isRegEx, methodPattern))
+                        );
+                    }
+
+                    @Override
+                    public AdviceListener getAdviceListener() {
+
+                        return new ReflectAdviceListenerAdapter.DefaultReflectAdviceListenerAdapter() {
+
+                            @Override
+                            public void create() {
+                                scriptListener.create(output);
+                            }
+
+                            @Override
+                            public void destroy() {
+                                scriptListener.destroy(output);
+                            }
+
+                            @Override
+                            public void before(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
+                                scriptListener.before(output, advice);
+                            }
+
+                            @Override
+                            public void afterReturning(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
+                                scriptListener.afterReturning(output, advice);
+                            }
+
+                            @Override
+                            public void afterThrowing(Advice advice, ProcessContext processContext, InnerContext innerContext) throws Throwable {
+                                scriptListener.afterThrowing(output, advice);
+                            }
+
+                        };
+                    }
+                };
             }
         };
     }
