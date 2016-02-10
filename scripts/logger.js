341180aac227d73e879ceb0a858273d289f5fa62
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2016-02-11 02:52:16 +0800

    调整格式

--- scripts/logger.js
+++ scripts/logger.js
@@ -27,7 +27,8 @@ Date.prototype.format = function (fmt) { //author: meizz
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        if (new RegExp("(" + k + ")").test(fmt)) fmt
+            = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
     return fmt;
 }
 
