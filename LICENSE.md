0f8f16605d6ea055690e0a57f10b7a0c0cf7b1d4
oldmanpushcart@gmail.com <oldmanpushcart@alibaba-inc.com>
2017-03-01 21:37:58 +0800

    clean

--- LICENSE.md
+++
@@ -1,3 +0,0 @@
-Copyright (c) 2011-2015 oldmanpushcart@gmail.com
-License: GNU General Public License 3 (GPLv3)
-Read more at http://shjs.sourceforge.net/doc/gplv3.html
--- core/src/main/java/com/github/ompc/greys/core/advisor/AdviceTracingListener.java
+++
@@ -1,8 +0,0 @@
-package com.github.ompc.greys.core.advisor;
-
-/**
- * 方法通知调用监听器
- * Created by oldmanpushcart@gmail.com on 15/5/27.
- */
-public interface AdviceTracingListener extends AdviceListener, InvokeTraceable {
-}
